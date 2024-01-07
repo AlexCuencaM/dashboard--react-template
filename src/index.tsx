0acae74d05000a3a21e2b6c4df24848d67a246c6
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2024-01-07 16:28:26 -0500

    Only type errors

--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,12 @@ import 'simplebar/src/simplebar.css';
 import { Provider as ReduxProvider } from 'react-redux';
 
 // apex-chart
-import 'assets/third-party/apex-chart.css';
+import './assets/third-party/apex-chart.css';
 
 // project import
 import App from './App';
-import { store } from 'store';
-import reportWebVitals from './reportWebVitals';
+import { store } from './store';
+// import reportWebVitals from './reportWebVitals';
 
 // ==============================|| MAIN - REACT DOM RENDER  ||============================== //
 
@@ -33,4 +33,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
